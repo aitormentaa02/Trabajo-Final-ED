53aba7d06e5b7f9bfe33376e6841837f5517ede8
AlvaroQuero <aqs0001@alu.medac.es>
2023-05-08 19:38:24 +0200

    mensaje de victoria

--- src/main/java/es/tictactoe/TicTacToeHandler.java
+++ src/main/java/es/tictactoe/TicTacToeHandler.java
@@ -98,6 +98,7 @@ public class TicTacToeHandler extends TextWebSocketHandler {
 
 			case JOIN_GAME:
 				int numPlayers = game.getPlayers().size();
+				/*cambio de la figura */
 				String letter = numPlayers == 0 ? "%" : "@";
 				Player player = new Player(numPlayers + 1, letter, msg.data.name);
 				game.addPlayer(player);
